ee5d829ce19cec77e538b2799b44d485c52258c3
Joshua <joshsmayhew@gmail.com>
2022-10-19 14:30:56 +0700

    create new state variables in FilmShow component and add useEffect functions to handle user film collection, conditionally pass props to GenButton

--- src/Components/FilmShow.js
+++ src/Components/FilmShow.js
@@ -1,21 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GenButton } from "./GenButton";
+import { collectionUrl } from "../Utilities/api-helpers";
+
+const initialCollection = {
+  collection: [],
+};
 
 function FilmShow({ films }) {
   const params = useParams();
   const [curFilm, setCurFilm] = useState(0);
   const [userCollection, setUserCollection] = useState([]);
-  const [alreadyInCOllection, setAlreadyInCollection] = useState(false);
+  const [alreadyInCollection, setAlreadyInCollection] = useState(false);
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetch(collectionUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("data: ", data);
+        const collection = data[0].collection;
+        setUserCollection(collection);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (params.filmId) {
+      setCurFilm(params.filmId);
+    }
+  }, [params]);
+
+  useEffect(() => {
+    const filmFound = userCollection.some(({ Title }) => {
+      return Title === films[curFilm].Title;
+    });
+
+    setAlreadyInCollection(filmFound);
+  }, [userCollection, curFilm, films]);
+
+  function handleRemoveFromCollection() {}
+
+  function handleAddToCollection() {}
 
   if (films[params.filmId])
     return (
       <div className="film-details">
         <h2>{films[params.filmId].Title}</h2>
         <p className="film-desc">{films[params.filmId].Description}</p>
-        <GenButton />
+        <GenButton
+          className={`collection-btn ${alreadyInCollection ? "remove" : "add"}`}
+          onClick={
+            alreadyInCollection
+              ? handleRemoveFromCollection
+              : handleAddToCollection
+          }
+          text={
+            alreadyInCollection ? "Remove from collection" : "Add to collection"
+          }
+        />
       </div>
     );
 }
--- src/Utilities/api-helpers.js
+++ src/Utilities/api-helpers.js
@@ -1,4 +1,5 @@
 const loginUrl = "http://localhost:3000/user-login";
 const criterionFilmUrl = "http://localhost:3000/criterion-database";
+const collectionUrl = "http://localhost:3000/user-collections";
 
-export { loginUrl, criterionFilmUrl };
+export { loginUrl, criterionFilmUrl, collectionUrl };
